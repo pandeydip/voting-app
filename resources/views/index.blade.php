45b192dad35341e3ce1369442553d03f8eb00761
Deepak Pandey <er.pandeydip@gmail.com>
2021-12-01 10:50:09 +0545

    Initial Design

---
+++ resources/views/index.blade.php
@@ -0,0 +1,30 @@
+<x-app-layout>
+    <div class="filters flex space-x-6">
+        <div class="w-1/3">
+            <select name="category" id="category" class="w-full rounded-xl border-none px-4 py-2">
+                <option value="Category One">Category One</option>
+                <option value="Category two">Category two</option>
+                <option value="Category three">Category three</option>
+                <option value="Category four">Category four</option>
+            </select>
+        </div>
+        <div class="w-1/3">
+            <select name="other_filters" id="category" class="w-full rounded-xl border-none px-4 py-2">
+                <option value="Category One">Category One</option>
+                <option value="Category two">Category two</option>
+                <option value="Category three">Category three</option>
+                <option value="Category four">Category four</option>
+            </select>
+        </div>
+        <div class="w-2/3 relative">
+            <input type="search" placeholder="Find an idea"
+                   class="placeholder-gray-900 w-full rounded-xl bg-white px-4 py-2 pl-8 border-none">
+            <div class="absolute top-0 flex items-center h-full ml-2">
+                <svg class="w-4 text-gray-700" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                          d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"/>
+                </svg>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
